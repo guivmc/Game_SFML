4f2004ffdf0009f489c39761002dbf236d807a8d
Guilherme Carvalhal <guivmc@hotmail.com>
2018-12-09 20:05:33 -0200

    Unit test project added

---
+++ MyGame/GameUnitTests/Engine_Tests.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../MyGame/Engine.h"
+
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GameUnitTests
+{
+	TEST_CLASS(Engine_Test)
+	{
+	public:
+
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: insira seu código de teste aqui
+		}
+
+	};
+}
---
+++ MyGame/GameUnitTests/unittest1.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GameUnitTests
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: insira seu código de teste aqui
+		}
+
+	};
+}
